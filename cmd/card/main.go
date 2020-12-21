8c94435edbccfd926644720d669e6dd04bf91d7e
Tilek Asankulov <tilekasankulov04@gmail.com>
2020-12-21 21:10:56 +0600

    Sum by month

--- cmd/card/main.go
+++ cmd/card/main.go
@@ -3,24 +3,55 @@ package main
 import (
 	"fmt"
 	"github.com/netwar1994/goroutines/pkg/card"
+	"math/rand"
+	"strconv"
+	"time"
 )
 
 func main() {
-	transaction1 := card.Transaction{
-		Id:  0,
-		Sum: 3000,
+	start := time.Date(2019, 1, 1, 0, 0, 0, 0, time.Local)
+	finish := time.Date(2020, 8, 1, 0, 0, 0, 0, time.Local)
+
+	months := make([]*card.Part, 0)
+
+	next := start
+	for next.Before(finish) {
+		months = append(months, &card.Part{
+			MonthTimestamp: next.Unix(),
+		})
+		next = next.AddDate(0, 1, 0)
 	}
-	transaction2 := card.Transaction{
-		Id:  1,
-		Sum: 2000,
-	}
-	transaction3 := card.Transaction{
-		Id:  2,
-		Sum: 22000,
+	months = append(months, &card.Part{
+		MonthTimestamp: finish.Unix(),
+	})
+
+
+	transactions := make([]int64, 10_00)
+	for transaction := range transactions{
+		transactions[transaction] = rand.Int63n(100)
 	}
 
-	transactions := []card.Transaction{transaction1, transaction2, transaction3}
-	fmt.Println("До сортировки:", transactions)
-	fmt.Println("После сортировки:", card.SortTransactions(transactions))
+
+	for _, transaction := range transactions {
+		month := months[rand.Intn(len(months))]
+		month.Transactions = append(month.Transactions, &card.Transaction{Sum: transaction})
+	}
+
+	monthsMap := make(map[string][]*card.Transaction)
+
+	for m := range months{
+		t :=  time.Unix(months[m].MonthTimestamp, 0)
+		year := t.Year()
+		month := t.Month().String()
+		d := strconv.Itoa(year) + "-" + month
+
+		monthsMap[d] = months[m].Transactions
+	}
+
+	fmt.Println("Transactions sum by month")
+	for m, t := range monthsMap {
+		fmt.Print(m, ":")
+		card.SumConcurrently(t, 1)
+	}
 
 }
--- pkg/card/card.go
+++ pkg/card/card.go
@@ -1,11 +1,19 @@
 package card
 
 import (
+	"fmt"
 	"sort"
+	"sync"
+	"time"
 )
 
+type Part struct {
+	MonthTimestamp int64
+	Transactions []*Transaction
+}
+
 type Transaction struct {
-	Id  int64
+	//Id  int64
 	Sum int64
 }
 
@@ -14,4 +22,39 @@ func SortTransactions(transactions []Transaction) []Transaction {
 		return transactions[i].Sum > transactions[j].Sum
 	})
 	return transactions
+}
+
+func Sum(transactions []*Transaction) int64 {
+	result := int64(0)
+	for _, transaction := range transactions{
+		result += transaction.Sum
+	}
+	return result
+}
+
+func SumConcurrently(transactions []*Transaction, goroutines int) int64 {
+	wg := sync.WaitGroup{}
+	wg.Add(goroutines)
+
+	total := int64(0)
+	partSize := len(transactions) / goroutines
+	for i := 0; i < goroutines; i++ {
+		// ВАЖНО: просто с partSize не прокатит, вам нужно как-то заранее разделить слайс по месяцам
+		// ВАЖНО: этот код - лишь шаблон, который показывает вам как запустить горутину
+		part := transactions[i*partSize : (i+1)*partSize]
+		go func() {
+			sum := Sum(part)
+			fmt.Println("sum:",  sum)
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	return total
+}
+
+func SumByMonth(period *Part)  {
+	fmt.Println(period.MonthTimestamp)
+	t := time.Unix(period.MonthTimestamp, 0)
+	fmt.Println(t.Month())
 }
